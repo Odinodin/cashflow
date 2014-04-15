4fc9ab6a8babe44bd6a16352f1dee8fa34a31eb0
Odin Hole Standal <odin@odinodin.com>
2014-04-15 15:22:22 +0200

    Init app state by getting transactions from backend through XHR

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -2,19 +2,18 @@
   (:require [goog.events :as events]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [acme.xhr :as xhr]))
 
 (enable-console-print!)
 
+;; Contains entire app state in an atom.
 (def app-state
   (atom
-    {:transactions
-      [{:description "General store" :amount 10 :tags ["store"]}
-       {:description "ACME cars" :amount 1 :tags ["car"]}]}))
+    {:transactions []}))
 
-
-(defn display-transaction [{:keys [description amount]}]
-  (str description " : " amount " NOK"))
+(defn display-transaction [{:keys [description amount code]}]
+  (str description " : " amount " NOK " "(" code ")"))
 
 (defn transaction-view [transaction owner]
   (reify
@@ -22,8 +21,17 @@
     (render [this]
       (dom/li nil (display-transaction transaction)))))
 
+
 (defn transactions-view [app owner]
   (reify
+    ;; Initialize state by retrieving transactions from the backend
+    om/IWillMount
+    (will-mount [_]
+      (xhr/json-xhr
+        {:method :get
+         :url "transactions"
+         :on-complete #_(print %) #(om/transact! app :transactions (fn [_] %))}))
+    ;; Render the transactions
     om/IRender
     (render [this]
       (dom/div nil
