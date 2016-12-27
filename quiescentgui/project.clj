b6121f6a38f5f776de96272063bbcb5d427715f1
Odin Standal <odin@odinodin.com>
2016-12-27 10:19:11 +0100

    Upgrade frontend libs for quiescent GUI

--- quiescentgui/project.clj
+++ quiescentgui/project.clj
@@ -1,21 +1,21 @@
 (defproject quiescentgui "0.0.1"
   :description "Cashflow quiscent gui"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.34"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.293"]
 
                  ;; Backend
-                 [compojure "1.5.0"]
+                 [compojure "1.5.1"]
                  [hiccup "1.0.5"]
                  [ring-server "0.4.0"]
                  [ring/ring-json "0.4.0"]
-                 [cheshire "5.3.1"]
-                 [clj-time "0.11.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]
+                 [cheshire "5.6.3"]
+                 [clj-time "0.12.2"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [clj-http "2.1.0"]
 
                  ;; Cljs
-                 [quiescent "0.3.1"]
-                 [cljs-http "0.1.39"]
+                 [quiescent "0.3.2"]
+                 [cljs-http "0.1.42"]
                  [secretary "1.2.3"]]
 
 
@@ -26,7 +26,7 @@
 
   :repl-options {:init-ns cashflow.repl}
 
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.10.0"]]
 
-  :profiles {:dev {:plugins [[lein-cljsbuild "1.1.3"]]
-                   :dependencies [[figwheel-sidecar "0.5.3-1"]]}})
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.1.5"]]
+                   :dependencies [[figwheel-sidecar "0.5.8"]]}})
